bfed704d2513b7e2e5c6c0c96059341b107ec991
Deyson Venté <deiandresvent@hotmail.com>
2024-10-01 09:08:20 -0500

    Se configuran las rutas de la aplicación.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((x) => x.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(
+        (x) => x.ContactComponent
+      ),
+  },
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./pages/users/users.component').then((x) => x.default),
+  },
+  {
+    path: 'user/:id',
+    loadComponent: () =>
+      import('./pages/user/user.component').then((x) => x.default),
+  },
+  {
+    path: 'frameworks',
+    loadComponent: () =>
+      import('./pages/layout/layout.component').then((x) => x.default),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/framework-list/framework-list.component').then(
+            (x) => x.FrameworkListComponent
+          ),
+      },
+      {
+        path: 'detalles',
+        loadComponent: () =>
+          import('./components/frameworks/frameworks.component').then(
+            (x) => x.FrameworksComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
