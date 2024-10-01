925287dbd703b6df24fc50428d4910b29bc6f7a0
Deyson Venté <deiandresvent@hotmail.com>
2024-10-01 09:07:23 -0500

    Variables en angular, tipos simple y con señales.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FrameworksComponent } from './components/frameworks/frameworks.component';
+import { NavbarComponent } from './shared/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FrameworksComponent, RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'myApp';
+  descripcion: string = 'Creando nuestra primera aplicación en angular v18';
+  isLive: boolean = true;
+  isOffline = signal(false);
+  frameworkName: string = 'Angular';
+  constructor() {}
+
+  onChangeName() {
+    this.frameworkName = 'Angular 18';
+    this.descripcion = 'Este es un directo sobre angular';
+  }
 }
